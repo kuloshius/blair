55527ae389f43f910651915006496974ccb97f6e
tomas <tomas@ebox.lt>
2011-01-09 23:06:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blair::Application.config.secret_token = '299c34d369e022636d40d8df05767987d26616d1187f3ef5d4dcf7f5bce82fe03deeaec735855a289bb4da410e03f4d23644a0db6b6dc4a366c2a26176243fe7'
